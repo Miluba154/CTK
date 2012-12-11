2c1123efde2bacf3c7e6ad43bd694beb3cb59d32
Michael Bauer <m.bauer@dkfz-heidelberg.de>
2012-12-11 13:05:16 +0100

    bologna

--- Libs/DICOM/Widgets/ctkDICOMQueryRetrieveWidget.h
+++ Libs/DICOM/Widgets/ctkDICOMQueryRetrieveWidget.h
@@ -49,7 +49,7 @@ public:
   QSharedPointer<ctkDICOMDatabase> retrieveDatabase()const;
 
   /// enable or disable ctk progress bars
-  void                   useProgressDialog(bool enable);
+  void useProgressDialog(bool enable);
 
 public Q_SLOTS:
   void setRetrieveDatabase(QSharedPointer<ctkDICOMDatabase> retrieveDatabase);
